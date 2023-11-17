299463230c3566ffe7787a175d6ab290abdac805
Kunlun Huang <hiplon@gmail.com>
2023-11-17 15:07:09 +0800

    submit a data encryption transmission example

---
+++ sources/LoRaReceiverChacha/LoRaReceiverChacha.ino
@@ -0,0 +1,107 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <ChaCha.h>
+#include <SPI.h>              // include libraries
+
+static const uint8_t key[32] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
+static const uint8_t iv[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
+
+const int csPin = 7;          // LoRa radio chip select
+const int resetPin = 6;       // LoRa radio reset
+const int irqPin = 1;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xAA;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+void setup() {
+  Serial.begin(9600);                   // initialize serial
+  while (!Serial);
+
+  Serial.println("LoRaKEY Duplex Receiver");
+
+  // override the default CS, reset, and IRQ pins (optional)
+  //LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  if (!LoRa.begin(868E6)) {             // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+
+  Serial.println("LoRa init succeeded.");
+}
+
+void loop() {
+  // parse for a packet, and call onReceive with the result:
+  onReceive(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  byte incomecrypt[incomingLength];
+
+  String incoming = "";
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+    //incomecrypt[count] = LoRa.read();
+  }
+  
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+  byte outputtext1[incomingLength];
+  byte buf[incomingLength];
+  incoming.getBytes(buf, incomingLength);
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  ChaCha chacha2(20);
+  chacha2.setKey(key,sizeof(key));
+  chacha2.setIV(iv,sizeof(iv));
+  chacha2.decrypt(outputtext1,buf,incomingLength);
+  Serial.println("RAW Transmission Buffer:" + String((char*)buf));
+  Serial.println("After Decrypt:");
+  //chacha2.decrypt(outputtext2,incoming,packetSize);
+  Serial.println((char*)outputtext1);
+  //Serial.println("DecryptStr: " + outputtext2);
+
+
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+  String message = String("Received");
+  sendMessage(message);
+}
+
